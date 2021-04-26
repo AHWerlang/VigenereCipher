aad5443cd5ff0f2b59a6af2763b0e00e30d4b377
Arthur Werlang <arthur.werlang@gmail.com>
2021-04-25 23:38:32 -0300

    First summit
    only encrypt part

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function encryptMessage() {
+	
+	var alphabetic = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "Á", "É", "Í", "Ó", "Ú", "Â", "Ê", "Ô", "Ã", "Õ", "À", "Ç"]
+	var enterText = document.getElementById("text-to-convert").value.toUpperCase();
+	var keyword = document.getElementById("keyword").value.toUpperCase();
+	var encryptMessage = "";
+	var f = 0;
+	var k = 0;
+
+	for (var i = 0; i < enterText.length; i++) {
+		if (f < keyword.length) {
+		
+		} else {
+			f = 0
+		}
+
+		if (alphabetic.indexOf(enterText[i]) == -1 || alphabetic.indexOf(keyword[f]) == -1) {
+			encryptMessage += enterText[i]
+		} else {
+			k = (alphabetic.indexOf(enterText[i]) + alphabetic.indexOf(keyword[f])) % 39; 
+			encryptMessage += alphabetic[k];
+		}
+	f++
+	}
+
+document.getElementById("encrypt-message").value = encryptMessage;
+}
